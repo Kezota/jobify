0380cca67d7c7dfa51c049ce6a6b282aeaf9d4d4
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2024-09-02 18:46:03 +0700

    update base url for production

--- client/src/utils/customFetch.ts
+++ client/src/utils/customFetch.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "https://jobify-server-c4ism34tl-kezotas-projects.vercel.app/api/v1",
+  baseURL: "https://jobify-server-5jcs5x4vn-kezotas-projects.vercel.app/api/v1",
 });
 
 export default customFetch;
