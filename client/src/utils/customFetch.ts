7a1ebffe91800da42391958e665cc887153f564c
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2024-09-02 17:38:58 +0700

    fix for deploy again

--- client/src/utils/customFetch.ts
+++ client/src/utils/customFetch.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL:
-    "https://jobify-server-mnzel09su-kezotas-projects.vercel.app//api/v1",
+  baseURL: "/api/v1",
 });
 
 export default customFetch;
