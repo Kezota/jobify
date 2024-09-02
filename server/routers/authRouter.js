a9a5fe63b086d26c76ee2add362ab440ec4d83e0
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2024-09-02 19:49:52 +0700

    edit router files

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -11,4 +11,15 @@ router.post("/register", validateRegisterInput, register);
 router.post("/login", validateLoginInput, login);
 router.get("/logout", logout);
 
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+});
+
 export default router;
